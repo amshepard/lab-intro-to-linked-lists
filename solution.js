74dc3c9dac6f217b75072171042ff4b06f90a2a0
Antoine M. Shepard <antoineshepard@pursuit.org>
2023-09-26 19:50:54 -0400

    Initial commit.

--- solution.js
+++ solution.js
@@ -1,5 +1,68 @@
 const { nums, words } = require("./data/data.js");
 
+class Node { 
+  constructor (data) {
+      this.data = data
+      this.next = null
+  }
+}
+
+
+class LinkedList {
+  constructor () {
+      this.head = null
+  }
+
+  insert (data) {
+      let newNode = new Node (data)
+      if (!this.head) {
+          this.head = newNode
+      } else {
+          newNode.next = this.head
+          this.head = newNode
+      }
+  }
+
+  getHeadData () {
+      return this.head.data
+  }
+
+
+  get3rd (k) {
+      let currentNode = this.head
+      currentNode = currentNode.next
+      currentNode = currentNode.next
+      return currentNode.data
+  }
+
+  getkth (k) {
+      let currentNode = this.head
+      for (let i = 0; i < k; i++) {  
+          if (currentNode.next) {
+              currentNode = currentNode.next
+          } else {
+              return undefined
+          }
+      }
+      return currentNode.data
+  }
+
+
+getLength () {
+  let count = 0
+  let currentNode = this.head
+  while (currentNode) {
+      count++
+      currentNode = currentNode.next
+  }
+  return count
+}
+
+getKthFromLast (k) {
+  return this.getKth(this.getLength() - k -1)
+}
+}
+
 module.exports = {
   Node,
   LinkedList,
